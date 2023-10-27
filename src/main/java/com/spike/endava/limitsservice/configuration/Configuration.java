729bca3152473b27442c141cc5a791a1b9834c3f
Leonardo Reyes <Leonardo.Reyes@endava.com>
2023-10-27 15:09:45 -0500

    Add automatic properties refresh with rabbitmq

--- src/main/java/com/spike/endava/limitsservice/configuration/Configuration.java
+++ src/main/java/com/spike/endava/limitsservice/configuration/Configuration.java
@@ -1,10 +1,12 @@
 package com.spike.endava.limitsservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("limits-service")
+@RefreshScope
 public class Configuration {
 
   private int minimum;
